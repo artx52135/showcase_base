1472d0a7373bc6f9b847c1df89394a99fb2512e4
Kertalo <koksharov-kir@yandex.ru>
2024-10-20 19:04:15 +0300

    first commit

---
+++ src/main/java/com/example/showcase/controllers/ProductsController.java
@@ -0,0 +1,13 @@
+package com.example.showcase.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class ProductsController {
+    @GetMapping("/")
+    String getProducts() {
+        return "products";
+    }
+}
