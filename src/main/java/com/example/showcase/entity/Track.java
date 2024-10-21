8cb645a5f97653a946b2d9bfd86e6238d13a8ed8
Artem <artx52135@gmail.com>
2024-10-21 22:26:44 +0300

    add table tracks

---
+++ src/main/java/com/example/showcase/entity/Track.java
@@ -0,0 +1,25 @@
+package com.example.showcase.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tracks")
+public class Track {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String track;
+
+    @Column(name = "closed")
+    private Boolean closed;
+}
