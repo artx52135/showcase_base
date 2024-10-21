167fe3ad57c10979c6e448ffcad3846d467448c1
Artem <artx52135@gmail.com>
2024-10-21 22:05:39 +0300

    add table users

---
+++ src/main/java/com/example/showcase/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.showcase.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String role;
+}
