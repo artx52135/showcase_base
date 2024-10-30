c97e513fe66cf8aa6d391ceafc850b5fa8aef383
Artem <artx52135@gmail.com>
2024-10-30 16:38:37 +0300

    add ProjectUserDTO

---
+++ src/main/java/com/example/showcase/dto/ProjectUserDTO.java
@@ -0,0 +1,11 @@
+package com.example.showcase.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProjectUserDTO {
+    private int projectId;
+    private int userId;
+}
